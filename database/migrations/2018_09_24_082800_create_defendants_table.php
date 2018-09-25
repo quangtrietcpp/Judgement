25e2b68795100d94461498af9ba23bf953baa554
quangtrietcpp <quangtrietpas@gmail.com>
2018-09-25 14:49:12 +0700

    Edit migrations

---
+++ database/migrations/2018_09_24_082800_create_defendants_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDefendantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('defendants', function (Blueprint $table) {
+            $table->string('defendant_id', 12);
+            $table->string('judgement_id', 12);
+            $table->primary('defendant_id', 'judgement_id');
+            $table->foreign('judgement_id')->references('judgement_id')->on('judgements');
+            $table->text('crime');
+            $table->date('date_of_birth');
+            $table->string('address');
+            $table->string('occupation');
+            $table->string('study_level');
+            $table->string('ethnics');
+            $table->string('religion');
+            $table->text('previous_conviction');
+            $table->text('antecedent');
+            $table->date('captured_on');
+            $table->boolean('present');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('defendants');
+    }
+}
