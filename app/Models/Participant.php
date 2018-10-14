482dbcfca28a6b2e878e8d74e9c653dda2fdf892
quangtrietcpp <quangtrietpas@gmail.com>
2018-10-14 11:29:33 +0700

    Add constants for models

--- app/Models/Participant.php
+++ app/Models/Participant.php
@@ -61,4 +61,46 @@ class Participant extends Model
     {
         return $this->belongsTo('App\Models\Judgement', 'judgement_id', 'judgement_id');
     }
+
+    const GENDER_MALE = 0;
+    const GENDER_FEMALE = 1;
+    const GENDER_OTHERS = 2;
+
+    public static $genders = [
+        self::GENDER_MALE => 'Nam',
+        self::GENDER_FEMALE => 'Nữ',
+        self::GENDER_OTHERS => 'Khác',
+    ];
+
+    const ROLE_REPRESENTOR_DEFENDANT = 0;
+    const ROLE_LAWYER_DEFENDANT = 1;
+    const ROLE_VICTIM = 2;
+    const ROLE_REPRESENTOR_VICTIM = 3;
+    const ROLE_LAWYER_VICTIM = 4;
+    const ROLE_CIVIL_PLAINTIFF = 5;
+    const ROLE_REPRESENTATOR_CIVIL_PLAINTIFF = 6;
+    const ROLE_LAWYER_CIVIL_PLAINTIFF = 7;
+    const ROLE_CIVIL_DEFENDANT = 8;
+    const ROLE_REPRESENTATOR_CIVIL_DEFENDANT = 9;
+    const ROLE_LAWYER_CIVIL_DEFENDANT = 10;
+    const ROLE_SUBORNER = 11;
+    const ROLE_RELATED_PERSON = 12;
+    const ROLE_OTHERS = 13;
+
+    public static $roles = [
+        self::ROLE_REPRESENTOR_DEFENDANT => 'Đại diện hợp pháp của bị cáo',
+        self::ROLE_LAWYER_DEFENDANT => 'Người bào chữa cho bị cáo',
+        self::ROLE_VICTIM => 'Bị hại',
+        self::ROLE_REPRESENTOR_VICTIM => 'Đại diện hợp pháp của bị hại',
+        self::ROLE_LAWYER_VICTIM => 'Người bào chữa cho bị hại',
+        self::ROLE_CIVIL_PLAINTIFF => 'Nguyên đơn dân sự',
+        self::ROLE_REPRESENTATOR_CIVIL_PLAINTIFF => 'Đại diện hợp pháp của nguyên đơn dân sự',
+        self::ROLE_LAWYER_CIVIL_PLAINTIFF => 'Người bào chữa cho nguyên đơn dân sự',
+        self::ROLE_CIVIL_DEFENDANT => 'Bị đơn dân sự',
+        self::ROLE_REPRESENTATOR_CIVIL_DEFENDANT => 'Đại diện hợp pháp của bị đơn dân sự',
+        self::ROLE_LAWYER_CIVIL_DEFENDANT => 'Người bào chữa cho bị đơn dân sự',
+        self::ROLE_SUBORNER => 'Người làm chứng',
+        self::ROLE_RELATED_PERSON => 'Người có quyền lợi, nghĩa vụ liên quan',
+        self::GENDER_OTHERS => 'Người tham gia tố tụng khác',
+    ];
 }
